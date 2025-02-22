5817887374bb263ee5bf53bc0224c9850eed25b2
realTPowell <tompowell213@gmail.com>
2025-02-22 16:03:17 +0000

    kickstart dependencies

--- configuration.nix
+++ configuration.nix
@@ -120,6 +120,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    gcc
+    make
+    unzip
     zoxide
     eza
     fzf
