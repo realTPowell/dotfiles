aaefce91246a69a7933ed401df8b116c3c031963
realTPowell <tompowell213@gmail.com>
2025-02-22 16:22:55 +0000

    drop make

--- configuration.nix
+++ configuration.nix
@@ -121,7 +121,6 @@
   environment.systemPackages = with pkgs; [
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     gcc
-    make
     unzip
     zoxide
     eza
