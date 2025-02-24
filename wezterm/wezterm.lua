c42dee9f42c4c1392b27de9407cf24480e5f86c1
realTPowell <tompowell213@gmail.com>
2025-02-24 20:14:29 +0000

    everforest in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 -- apply config choices
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = 'Everforest Dark'
 config.font = wezterm.font('Iosevka Nerd Font')
 
 -- finally, return the built config to Wezterm
