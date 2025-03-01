59769b1129dba490d2f216b25eab990ed7c3dc40
realTPowell <tompowell213@gmail.com>
2025-03-01 20:11:56 +0000

    add telescope picker to obsidian

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -23,6 +23,25 @@ return {
       folder = "99 - Assets/Templates",
       date_format = "%Y-%m-%d",
       time_format = "%H:%M",
-    }
+    },
+    
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "telescope.nvim",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      note_mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+      tag_mappings = {
+        -- Add tag(s) to current note.
+        tag_note = "<C-x>",
+        -- Insert a tag at the current location.
+        insert_tag = "<C-l>",
+      },
+    },
   },
 }
